9724c8f7a7c49cc5e3ee685dcf54d8f7411a9aee
suisrc <suisrc@outlook.com>
2026-01-21 14:05:18 +0800

    fix front2

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"embed"
 	_ "embed"
-	"io/fs"
 	"strings"
 
-	"github.com/suisrc/zgg/app/front2"
 	"github.com/suisrc/zgg/z"
 	_ "github.com/suisrc/zgg/z/ze/rdx"
 
@@ -24,8 +21,8 @@ var app_ []byte
 //go:embed version
 var ver_ []byte
 
-//go:embed www/* www/**/*
-var www_ embed.FS // all:www/* all:www/**/*
+// //go:embed www/* www/**/*
+// var www_ embed.FS // all:www/* all:www/**/*
 
 /**
  * 程序入口
@@ -37,8 +34,8 @@ func main() {
 	// zc.C.Syslog, zc.C.LogTty = "udp://klog.default.svc:5141", false
 	// z.HttpServeDef = false // 标记是否启动默认 HTTP 服务， z.RegisterDefaultHttpServe
 
-	_www, _ := fs.Sub(www_, "www")
-	front2.Init3(_www, nil) // 前端应用，由于需要 wwwFS参数，必须人工初始化
+	// _www, _ := fs.Sub(www_, "www")
+	// front2.Init3(_www, nil) // 前端应用，由于需要 wwwFS参数，必须人工初始化
 	// front2.Init3(os.DirFS("www"), nil) // 前端应用, 使用系统文件夹中的文件
 	// kwlog2.Init3(nil) // 采集器日志, 为 fluentbit agent 提供 HTTP 收集日志功能
 	// kwdog2.Init3(nil) // API反向网关， 通过 Sidecar 模式保护内部服务
