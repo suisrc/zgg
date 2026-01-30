f6abc21eb7d284cf45f1c9dea6fbaa46d15861a7
suisrc <suisrc@outlook.com>
2026-01-30 11:40:48 +0800

    fix env map

--- z/zc/env.go
+++ z/zc/env.go
@@ -11,7 +11,6 @@ package zc
 import (
 	"os"
 	"reflect"
-	"strconv"
 	"strings"
 )
 
@@ -40,14 +39,26 @@ func (aa *ENV) Decode(val any, tag string) error {
 		if val, err := StrToBV(tag.Field.Type, venv); err == nil {
 			tag.Value.Set(reflect.ValueOf(val))
 		} else if vkey[len(vkey)-1] == 'S' {
-			idx := -1
+			// 此方案必须保障配置的联系性， 否则， 配置项将无法正确处理
+			// for idx := 0; ; idx++ {
+			// 	if venv = os.Getenv(vkey + "_" + strconv.Itoa(idx)); venv == "" {
+			// 		break
+			// 	}
+			// 	arr = append(arr, venv)
+			// }
+			//
+			// 判断 vkey_0 是否存在，如果存在，处理所有的环境变了，查询已 vkey_开头的参数，进行处理
+			// 这种方式的优势在于， 需要确定 vkey_0 存在， 并且需要执行集合处理，尽量避免过多次遍历
 			arr := []string{}
-			for {
-				idx++
-				if venv = os.Getenv(vkey + "_" + strconv.Itoa(idx)); venv == "" {
-					break
+			if venv = os.Getenv(vkey + "_0"); venv != "" {
+				for _, vv := range os.Environ() {
+					if strings.HasPrefix(vv, vkey+"_") {
+						vv2 := strings.SplitN(vv, "=", 2)
+						if len(vv2) == 2 && vv2[1] != "" {
+							arr = append(arr, vv2[1])
+						}
+					}
 				}
-				arr = append(arr, venv)
 			}
 			if len(arr) > 0 {
 				if tag.Field.Type.Kind() == reflect.Map && //
@@ -68,29 +79,29 @@ func (aa *ENV) Decode(val any, tag string) error {
 						}
 					}
 					tag.Value.Set(reflect.ValueOf(vvv))
-
-					// } else if tag.Field.Type.Kind() == reflect.Slice && //
-					// 	tag.Field.Type.Elem().Len() == 2 && //
-					// 	tag.Field.Type.Elem().Elem().Kind() == reflect.String {
-					// 	// tag.Field.Type = [][2]string
-					// 	vvv := [][2]string{}
-					// 	for _, vv := range arr {
-					// 		vv = strings.TrimSpace(vv)
-					// 		if vv == "" {
-					// 			continue
-					// 		}
-					// 		kv := strings.SplitN(vv, "=", 2)
-					// 		if len(kv) == 2 {
-					// 			vvv = append(vvv, [2]string{kv[0], kv[1]})
-					// 		} else {
-					// 			vvv = append(vvv, [2]string{vv, ""})
-					// 		}
-					// 	}
-					// 	tag.Value.Set(reflect.ValueOf(vvv))
-
 				} else if val, err := ToBasicValue(tag.Field.Type, arr); err == nil {
 					tag.Value.Set(reflect.ValueOf(val))
 				}
+				// 由于原始的MAP是没有顺序的，这里强调的是一种有续的map形式， 功能待定
+				// else if tag.Field.Type.Kind() == reflect.Slice && //
+				// 	tag.Field.Type.Elem().Len() == 2 && //
+				// 	tag.Field.Type.Elem().Elem().Kind() == reflect.String {
+				// 	// tag.Field.Type = [][2]string
+				// 	vvv := [][2]string{}
+				// 	for _, vv := range arr {
+				// 		vv = strings.TrimSpace(vv)
+				// 		if vv == "" {
+				// 			continue
+				// 		}
+				// 		kv := strings.SplitN(vv, "=", 2)
+				// 		if len(kv) == 2 {
+				// 			vvv = append(vvv, [2]string{kv[0], kv[1]})
+				// 		} else {
+				// 			vvv = append(vvv, [2]string{vv, ""})
+				// 		}
+				// 	}
+				// 	tag.Value.Set(reflect.ValueOf(vvv))
+				// }
 			}
 		}
 	}
